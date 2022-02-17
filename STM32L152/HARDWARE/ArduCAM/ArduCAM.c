9fd87c18056e33a61ae2a014f7bdbea09cd62142
ljp-while <1964503412@qq.com>
2022-02-17 16:19:14 +0800

    Fix BMP format error problem

--- STM32L152/HARDWARE/ArduCAM/ArduCAM.c
+++ STM32L152/HARDWARE/ArduCAM/ArduCAM.c
@@ -88,12 +88,6 @@ void ArduCAM_Init(byte model)
         }
 				write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH					
 				
-				uint8_t _x3503;
-				wrSensorReg16_8(0x5001,_x3503|0x01);	 // Close auto exposure mode
-	      			//Manually set the exposure value 
-				wrSensorReg16_8(0x3500,0x00);
-				wrSensorReg16_8(0x3501,0x79);
-				wrSensorReg16_8(0x3502,0xe0);		
 			  break;
       }
      default:
